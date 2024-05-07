fd25cf469cc01a09a0cf65765316de12209d7219
SegusFaultise <wilsonzakk8@gmail.com>
2024-05-07 18:41:54 +1000

    misc

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "include/parse_nip_files.h"
 
 #define TEST_FILE_PATH "../main.nip"
+
+void split_token_stream(char *file_contents) {
+    for(int i = 0; i < strlen(file_contents); i++) { 
+        if(file_contents == NULL) {
+            break;
+        }
+
+        while(isalpha(file_contents[i])) {
+            printf("%c\n", file_contents[i]);
+
+            break;
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     printf("main() HIT!\n\n");
@@ -11,6 +27,7 @@ int main(int argc, char **argv) {
 
     printf("%s", file_contents);
     
-
+    split_token_stream(file_contents);
+    
     return 0;
 }
--- src/parse_nip_files.c
+++ src/parse_nip_files.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -8,15 +8,25 @@ struct FileData {
     char *file_buffer;
 };
 
+long get_file_size(char *filename) {
+    struct stat file_status;
+
+    if (stat(filename, &file_status) < 0) {
+        return -1;
+    }
+
+    return file_status.st_size;
+}
+
 char *read_in_nip_file(char *file_path) {
     struct FileData file_data;
 
     int file_descriptor = open(file_path, O_RDONLY);
     int nb_read = -1;
+    
+    long file_size = get_file_size(file_path);
 
-    file_data.file_buffer = (char*)malloc(sizeof(file_descriptor));
-
-    char *file_contents;
+    file_data.file_buffer = (char*)malloc(sizeof(file_size));
 
     if(file_descriptor == -1) {
         printf("Error cannot find .nip file! %d", file_descriptor);
@@ -24,7 +34,7 @@ char *read_in_nip_file(char *file_path) {
     }
 
     while(nb_read != 0) {
-        nb_read = read(file_descriptor, file_data.file_buffer, 100); 
+        nb_read = read(file_descriptor, file_data.file_buffer, file_size); 
 
         if(nb_read == -1) {
             printf("Error cannot read in .nip file %d", nb_read);
@@ -38,3 +48,5 @@ char *read_in_nip_file(char *file_path) {
 
     return file_data.file_buffer;
 }
+
+
