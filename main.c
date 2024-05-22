39911d2b9b0fa16c0367c00bcff81cdfce0b2b69
SegusFaultise <wilsonzakk8@gmail.com>
2024-05-22 15:11:15 +1000

    Tokenizer no runs fast as fuck with fork(), big ups to unistd.h

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "include/parse_nip_files.h"
 
@@ -14,6 +17,10 @@
 #define STRING_IGNORES "\t\n "
 #define RAW_FILE_STRING_PAD 244
 
+#define IS_CHILD_PROCESS 0
+
+#define DEBUG_STATMENT printf("DEBUG_IS_HERE\n");
+
 char *extract_alpha(char *token_stream, size_t *token_index, size_t *token_stream_length) {
     int start = *token_index;
     int alpha_length = 0;
@@ -21,7 +28,7 @@ char *extract_alpha(char *token_stream, size_t *token_index, size_t *token_strea
     char *alpha = NULL;
 
     while(isalpha(token_stream[*token_index]) ||
-          isalpha(token_stream[*token_index] || token_stream[*token_index] == '_')) {
+            isalpha(token_stream[*token_index] || token_stream[*token_index] == '_')) {
 
         (*token_index)++;
         alpha_length++;
@@ -35,9 +42,9 @@ char *extract_alpha(char *token_stream, size_t *token_index, size_t *token_strea
 
             return NULL;
         }
-
         memcpy(alpha, token_stream + start, alpha_length);
         alpha[alpha_length] = '\0'; 
+
         return alpha;
     }
     else {
@@ -47,27 +54,33 @@ char *extract_alpha(char *token_stream, size_t *token_index, size_t *token_strea
 
 void build_token_stream_threads(char *raw_file_string, size_t raw_file_string_size) {
     size_t token_index = 0;
+    pid_t p_fork = fork();
+
 
     if(raw_file_string == NULL || 
-        token_index < 0 || 
-        raw_file_string[token_index] == '\0') {
+            token_index < 0 || 
+            raw_file_string[token_index] == '\0') {
 
         fprintf(stderr, "Error: The giving [raw_file_string] is NULL!");
         exit(-1);
     }
-
+    if(p_fork<0) {
+        fprintf(stderr, "Error: fork() failed!");
+        exit(1);
+    }
+    else if(p_fork == IS_CHILD_PROCESS)
     while(token_index < raw_file_string_size - 1) {
         if(token_index > raw_file_string_size) break;
 
         if(ispunct(raw_file_string[token_index])) {
-            printf("TOKEN_SYMBOL: %c\n", raw_file_string[token_index]);
+            //printf("TOKEN_SYMBOL: %c\n", raw_file_string[token_index]);
         }
         if(isdigit(raw_file_string[token_index])) {
-            printf("TOKEN_DIGIT: %c\n", raw_file_string[token_index]);
+            //printf("TOKEN_DIGIT: %c\n", raw_file_string[token_index]);
         }
         if(isalpha(raw_file_string[token_index])){
-            printf("TOKEN_ALPHA: %s\n", extract_alpha(raw_file_string, &token_index, &raw_file_string_size));
-            //extract_alpha(raw_file_string, &token_index, &raw_file_string_size);
+            //printf("TOKEN_ALPHA: %s\n", extract_alpha(raw_file_string, &token_index, &raw_file_string_size));
+            extract_alpha(raw_file_string, &token_index, &raw_file_string_size);
         }
         else {
             token_index++;
@@ -80,8 +93,8 @@ int main(int argc, char **argv) {
     printf("main() HIT!\n\n");
 
 
-    char *file_contents = read_in_nip_file(TEST_FILE_PATH);
-    size_t file_size = get_file_size(TEST_FILE_PATH);
+    char *file_contents = read_in_nip_file(TEST2_FILE_PATH);
+    size_t file_size = get_file_size(TEST2_FILE_PATH);
 
     //printf("[PARSED NIP FILE] %s\n", file_contents);
 
